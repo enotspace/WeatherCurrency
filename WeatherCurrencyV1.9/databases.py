4b53bb846563aac9393179f833eec1573f123488
enotspace <90465542+enotspace@users.noreply.github.com>
2023-10-10 21:47:53 +0300

    Add files via upload

---
+++ WeatherCurrencyV1.9/databases.py
@@ -0,0 +1,9 @@
+from settings import *
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=True)
+    text = db.Column(db.Text(300), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    def __repr__(self):
+        return '<Article%r>' % self.id
